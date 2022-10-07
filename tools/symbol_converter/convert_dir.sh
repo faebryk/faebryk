65d3f3dd0b854172944e900df96d4da9ab541774
IoannisP-ITENG <ioannis@iteng.nl>
2022-10-07 16:11:02 +0200

    Bugfix: Tools: Symbol converter module names

--- tools/symbol_converter/convert_dir.sh
+++ tools/symbol_converter/convert_dir.sh
@@ -6,7 +6,7 @@ mkdir -p $out
 
 run() {
     i=$1
-    name=$(basename $i .kicad_sym)
+    name=$(basename $i .kicad_sym | sed -r "s/^([^a-zA-Z])/_\1/")
     echo $name
     python3 $(dirname $0)/main.py $i $out/$name.py || echo "Failed running for $i" && exit 1
 } 

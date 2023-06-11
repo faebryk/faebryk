517ae1ef8e2631606b07b7143e8621d082772b82
ruben-iteng <94007802+ruben-iteng@users.noreply.github.com>
2023-06-11 15:51:36 +0200

    Library: BJT: add doping type and can bridge trait

--- src/faebryk/library/library/electronic/base/bjt.py
+++ src/faebryk/library/library/electronic/base/bjt.py
@@ -1,8 +1,10 @@
+from enum import Enum
 import logging
 
 from faebryk.library.core import Component
 from faebryk.library.library.interfaces import Electrical
 from faebryk.library.trait_impl.component import (
+    can_bridge_defined,
     has_defined_type_description,
 )
 
@@ -10,13 +12,30 @@ logger = logging.getLogger("BJT")
 
 
 class BJT(Component):
+    class DopingType(Enum):
+        NPN = 1
+        PNP = 2
+
+    # TODO use this, here is more info: https://en.wikipedia.org/wiki/Bipolar_junction_transistor#Regions_of_operation
+    class OperationRegion(Enum):
+        ACTIVE = 1
+        INVERTED = 2
+        SATURATION = 3
+        CUT_OFF = 4
+
     def __new__(cls):
         self = super().__new__(cls)
         self._setup_traits()
         return self
 
-    def __init__(self) -> None:
+    def __init__(
+        self, doping_type: DopingType, operation_region: OperationRegion
+    ) -> None:
         super().__init__()
+
+        self.doping_type = doping_type
+        self.operation_region = operation_region
+
         self._setup_interfaces()
 
     def _setup_traits(self):
@@ -29,3 +48,7 @@ class BJT(Component):
             collector = Electrical()
 
         self.IFs = _IFs(self)
+        # TODO pretty confusing
+        self.add_trait(
+            can_bridge_defined(in_if=self.IFs.collector, out_if=self.IFs.emitter)
+        )

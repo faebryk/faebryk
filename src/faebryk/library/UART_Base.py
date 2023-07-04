55a169735a79f99df62cedf6ffcb01e2e9ddd4ee
IoannisP-ITENG <ioannis@iteng.nl>
2023-07-04 09:37:36 +0200

    Make uart consistent

--- src/faebryk/library/UART_Base.py
+++ src/faebryk/library/UART_Base.py
@@ -3,12 +3,12 @@ from faebryk.library.Electrical import Electrical
 
 
 class UART_Base(ModuleInterface):
-    class NODES(ModuleInterface.NODES()):
-        rx = Electrical()
-        tx = Electrical()
-        gnd = Electrical()
-
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+
+        class NODES(ModuleInterface.NODES()):
+            rx = Electrical()
+            tx = Electrical()
+            gnd = Electrical()
 
         self.NODEs = UART_Base.NODES(self)

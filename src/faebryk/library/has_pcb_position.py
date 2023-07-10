94f3fa3d674ee6078d83c01d5af3fb3bbd6cc2e2
IoannisP-ITENG <ioannis@iteng.nl>
2023-07-10 10:17:58 +0200

    Split has_pcb_position into files

--- src/faebryk/library/has_pcb_position.py
+++ src/faebryk/library/has_pcb_position.py
@@ -7,25 +7,14 @@ from enum import Enum
 from faebryk.core.core import ModuleTrait
 
 
-class layer_type(Enum):
-    NONE = 1
-    TOP_LAYER = 2
-    BOTTOM_LAYER = 3
-
-
-Point = tuple[float, float]
-
-
 class has_pcb_position(ModuleTrait):
+    class layer_type(Enum):
+        NONE = 1
+        TOP_LAYER = 2
+        BOTTOM_LAYER = 3
+
+    Point = tuple[float, float]
+
     @abstractmethod
     def get_position(self) -> Point:
         ...
-
-
-class has_pcb_position_defined(has_pcb_position.impl()):
-    def __init__(self, position: Point) -> None:
-        super().__init__()
-        self.position = position
-
-    def get_position(self) -> Point:
-        return self.position

2de8e61df4e807f8a2266f49031281e59ef89cff
ruben-iteng <94007802+ruben-iteng@users.noreply.github.com>
2023-12-04 11:37:27 +0100

    Add esphome bus_id
    Co-authored-by: Ioannis <IoannisPZP@users.noreply.github.com>

--- src/faebryk/library/has_esphome_config.py
+++ src/faebryk/library/has_esphome_config.py
@@ -29,14 +29,23 @@ class has_esphome_config_defined(has_esphome_config.impl()):
 class is_esphome_bus(ModuleInterfaceTrait):
     ...
 
+    @abstractmethod
+    def get_bus_id(self) -> str:
+        ...
+
     @staticmethod
     def find_connected_bus(bus: ModuleInterface):
         connected_mifs = bus.get_direct_connections()
         try:
-            return find(
-                connected_mifs,
-                lambda mif: mif.has_trait(is_esphome_bus)
-                and mif.has_trait(has_esphome_config),
-            )
+            return find(connected_mifs, lambda mif: mif.has_trait(is_esphome_bus))
         except ValueError:
             raise Exception(f"No esphome bus connected to {bus}: {connected_mifs}")
+
+
+class is_esphome_bus_defined(is_esphome_bus.impl()):
+    def __init__(self, bus_id: str):
+        super().__init__()
+        self._bus_id = bus_id
+
+    def get_bus_id(self) -> str:
+        return self._bus_id

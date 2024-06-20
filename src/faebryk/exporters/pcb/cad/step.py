1dc992ed17e6f0e6ea86e7be66af59e37b08263f
ruben-iteng <94007802+ruben-iteng@users.noreply.github.com>
2024-06-20 17:40:17 +0200

    Add installation and version checks

--- src/faebryk/exporters/pcb/cad/step.py
+++ src/faebryk/exporters/pcb/cad/step.py
@@ -3,6 +3,7 @@
 
 import logging
 import os
+import re
 import subprocess as sp
 from pathlib import Path
 
@@ -19,6 +20,21 @@ def export_step(pcb_file: Path, path: Path):
     logger.info(f"Exporting step file to {path}")
     if not path.parent.exists():
         os.makedirs(path.parent)
+
+    # check if kicad-cli 8 is installed
+    try:
+        sp.check_output("which kicad-cli", shell=True)
+    except sp.CalledProcessError:
+        raise Exception("kicad-cli is not installed")
+
+    # check if kicad-cli is version 8
+    try:
+        version = sp.check_output("kicad-cli --version", shell=True).decode("utf-8")
+        if not re.search(r"8\.\d+\.\d+", version):
+            raise Exception(f"kicad-cli is not version 8.x.x but version {version}")
+    except sp.CalledProcessError:
+        raise Exception("Failed to check kicad-cli version")
+
     try:
         sp.check_output(
             f"kicad-cli pcb export step --force --no-dnp --subst-models --output {path} {pcb_file}",

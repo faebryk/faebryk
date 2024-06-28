11e543c728b8623d5851d1dfbef6a4151c9af028
IoannisP_ITENG <ioannis@iteng.nl>
2024-06-28 20:02:03 +0200

    Example: Add PCB transform

---
+++ examples/pcb_layout.py
@@ -0,0 +1,53 @@
+# This file is part of the faebryk project
+# SPDX-License-Identifier: MIT
+
+"""
+This file contains a faebryk sample.
+"""
+
+import logging
+
+import faebryk.library._F as F
+import typer
+from faebryk.core.core import Module
+from faebryk.libs.brightness import TypicalLuminousIntensity
+from faebryk.libs.experiments.buildutil import (
+    tag_and_export_module_to_netlist,
+)
+from faebryk.libs.logging import setup_basic_logging
+
+logger = logging.getLogger(__name__)
+
+
+class App(Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+        class _NODES(Module.NODES()):
+            led = F.PoweredLED()
+            battery = F.Battery()
+
+        self.NODEs = _NODES(self)
+
+        self.NODEs.led.IFs.power.connect(self.NODEs.battery.IFs.power)
+
+        # Parametrize
+        self.NODEs.led.NODEs.led.PARAMs.color.merge(F.LED.Color.YELLOW)
+        self.NODEs.led.NODEs.led.PARAMs.brightness.merge(
+            TypicalLuminousIntensity.APPLICATION_LED_INDICATOR_INSIDE.value.value
+        )
+
+
+def main():
+    logger.info("Building app")
+    app = App()
+
+    logger.info("Export")
+    tag_and_export_module_to_netlist(app, pcb_transform=True)
+
+
+if __name__ == "__main__":
+    setup_basic_logging()
+    logger.info("Running experiment")
+
+    typer.run(main)

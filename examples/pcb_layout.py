0685c299856fbf44955125114d5bda7ea531aee2
Jasper Smit <git@jrhrsmit.nl>
2024-06-29 09:48:25 +0200

    wip: Implement FontLayout, try to fix example

--- examples/pcb_layout.py
+++ examples/pcb_layout.py
@@ -6,10 +6,12 @@ This file contains a faebryk sample.
 """
 
 import logging
+from pathlib import Path
 
 import faebryk.library._F as F
 import typer
 from faebryk.core.core import Module
+from faebryk.exporters.pcb.kicad.layout.font import FontLayout
 from faebryk.exporters.pcb.kicad.layout.simple import SimpleLayout
 from faebryk.library.has_pcb_layout_defined import has_pcb_layout_defined
 from faebryk.library.has_pcb_position import has_pcb_position
@@ -19,54 +21,66 @@ from faebryk.libs.experiments.buildutil import (
     tag_and_export_module_to_netlist,
 )
 from faebryk.libs.logging import setup_basic_logging
+from faebryk.libs.util import times
 
 logger = logging.getLogger(__name__)
+
+
+class LEDText(Module):
+    def __init__(self, num_leds: int) -> None:
+        super().__init__()
+
+        class _IFs(Module.IFS()):
+            power = F.ElectricPower()
+
+        self.IFs = _IFs(self)
+
+        class _NODES(Module.NODES()):
+            leds = times(
+                num_leds,
+                F.PoweredLED,
+            )
+
+        self.NODEs = _NODES(self)
+
+        for led in self.NODEs.leds:
+            led.IFs.power.connect(self.IFs.power)
+            # Parametrize
+            led.NODEs.led.PARAMs.color.merge(F.LED.Color.YELLOW)
+            #led.NODEs.led.PARAMs.brightness.merge(
+            #    TypicalLuminousIntensity.APPLICATION_LED_INDICATOR_INSIDE.value.value
+            #)
 
 
 class App(Module):
     def __init__(self) -> None:
         super().__init__()
 
+        led_layout = FontLayout(
+            ttf=Path("Minecraftia-Regular.ttf"),
+            text="F",
+            char_dimensions=(5, 7),
+            resolution=(1, 1),
+        )
+
         class _NODES(Module.NODES()):
-            led = F.PoweredLED()
+            leds = LEDText(led_layout.get_count())
             battery = F.Battery()
 
         self.NODEs = _NODES(self)
 
-        self.NODEs.led.IFs.power.connect(self.NODEs.battery.IFs.power)
+        self.NODEs.leds.IFs.power.connect(self.NODEs.battery.IFs.power)
 
-        # Parametrize
-        self.NODEs.led.NODEs.led.PARAMs.color.merge(F.LED.Color.YELLOW)
-        self.NODEs.led.NODEs.led.PARAMs.brightness.merge(
-            TypicalLuminousIntensity.APPLICATION_LED_INDICATOR_INSIDE.value.value
-        )
+        led_layout.apply(self.NODEs.leds)
 
         # Layout
         Point = has_pcb_position.Point
         L = has_pcb_position.layer_type
 
-        # Powered led layout
-        self.NODEs.led.add_trait(
-            has_pcb_layout_defined(
-                SimpleLayout(
-                    layouts=[
-                        SimpleLayout.SubLayout(
-                            mod_type=F.LED,
-                            position=Point((0, 0, 0, L.TOP_LAYER)),
-                        ),
-                        SimpleLayout.SubLayout(
-                            mod_type=F.Resistor,
-                            position=Point((10, 0, 0, L.TOP_LAYER)),
-                        ),
-                    ]
-                )
-            )
-        )
-
         layout = SimpleLayout(
             layouts=[
                 SimpleLayout.SubLayout(
-                    mod_type=F.PoweredLED,
+                    mod_type=LEDText,
                     position=Point((0, 0, 0, L.TOP_LAYER)),
                 ),
                 SimpleLayout.SubLayout(

f164995c1bf1e727279debbb4a06a8a57f61a657
IoannisP_ITENG <ioannis@iteng.nl>
2024-07-11 11:57:17 +0200

    Add full project

--- src/faebryk/libs/kicad/fileformats.py
+++ src/faebryk/libs/kicad/fileformats.py
@@ -2,17 +2,23 @@ import logging
 from dataclasses import dataclass, field
 from enum import StrEnum, auto
 from pathlib import Path
-from typing import Optional
+from typing import Any, Optional
 
 from dataclasses_json import CatchAll, Undefined, dataclass_json
 from faebryk.libs.sexp.dataclass_sexp import JSON_File, SEXP_File, sexp_field
 
 logger = logging.getLogger(__name__)
 
+# TODO find complete examples of the fileformats, maybe in the kicad repo
+
 
 @dataclass_json(undefined=Undefined.INCLUDE)
 @dataclass
 class C_kicad_project_file(JSON_File):
+    """
+    Generated with ChatGPT by giving it the test.kicad_pro
+    """
+
     @dataclass_json(undefined=Undefined.INCLUDE)
     @dataclass
     class C_pcbnew:
@@ -31,16 +37,436 @@ class C_kicad_project_file(JSON_File):
             unknown: CatchAll = None
 
         last_paths: C_last_paths = field(default_factory=C_last_paths)
+        page_layout_descr_file: str = ""
         unknown: CatchAll = None
 
     pcbnew: C_pcbnew = field(default_factory=C_pcbnew)
+
+    @dataclass_json(undefined=Undefined.INCLUDE)
+    @dataclass
+    class C_board:
+        @dataclass_json(undefined=Undefined.INCLUDE)
+        @dataclass
+        class C_design_settings:
+            @dataclass_json(undefined=Undefined.INCLUDE)
+            @dataclass
+            class C_defaults:
+                apply_defaults_to_fp_fields: bool = False
+                apply_defaults_to_fp_shapes: bool = False
+                apply_defaults_to_fp_text: bool = False
+                board_outline_line_width: float = 0.05
+                copper_line_width: float = 0.2
+                copper_text_italic: bool = False
+                copper_text_size_h: float = 1.5
+                copper_text_size_v: float = 1.5
+                copper_text_thickness: float = 0.3
+                copper_text_upright: bool = False
+                courtyard_line_width: float = 0.05
+                dimension_precision: int = 4
+                dimension_units: int = 3
+
+                @dataclass_json(undefined=Undefined.INCLUDE)
+                @dataclass
+                class C_dimensions:
+                    arrow_length: int = 1270000
+                    extension_offset: int = 500000
+                    keep_text_aligned: bool = True
+                    suppress_zeroes: bool = False
+                    text_position: int = 0
+                    units_format: int = 1
+                    unknown: CatchAll = None
+
+                dimensions: C_dimensions = field(default_factory=C_dimensions)
+                fab_line_width: float = 0.1
+                fab_text_italic: bool = False
+                fab_text_size_h: float = 1.0
+                fab_text_size_v: float = 1.0
+                fab_text_thickness: float = 0.15
+                fab_text_upright: bool = False
+                other_line_width: float = 0.1
+                other_text_italic: bool = False
+                other_text_size_h: float = 1.0
+                other_text_size_v: float = 1.0
+                other_text_thickness: float = 0.15
+                other_text_upright: bool = False
+
+                @dataclass_json(undefined=Undefined.INCLUDE)
+                @dataclass
+                class C_pads:
+                    drill: float = 0.762
+                    height: float = 1.524
+                    width: float = 1.524
+                    unknown: CatchAll = None
+
+                pads: C_pads = field(default_factory=C_pads)
+                silk_line_width: float = 0.1
+                silk_text_italic: bool = False
+                silk_text_size_h: float = 1.0
+                silk_text_size_v: float = 1.0
+                silk_text_thickness: float = 0.1
+                silk_text_upright: bool = False
+
+                @dataclass_json(undefined=Undefined.INCLUDE)
+                @dataclass
+                class C_zones:
+                    min_clearance: float = 0.5
+                    unknown: CatchAll = None
+
+                zones: C_zones = field(default_factory=C_zones)
+                unknown: CatchAll = None
+
+            defaults: C_defaults = field(default_factory=C_defaults)
+
+            @dataclass_json(undefined=Undefined.INCLUDE)
+            @dataclass
+            class C_diff_pair_dimensions:
+                unknown: CatchAll = None
+
+            diff_pair_dimensions: list[C_diff_pair_dimensions] = field(
+                default_factory=list
+            )
+
+            @dataclass_json(undefined=Undefined.INCLUDE)
+            @dataclass
+            class C_drc_exclusions:
+                unknown: CatchAll = None
+
+            drc_exclusions: list[C_drc_exclusions] = field(default_factory=list)
+
+            @dataclass_json(undefined=Undefined.INCLUDE)
+            @dataclass
+            class C_meta:
+                version: int = 2
+                unknown: CatchAll = None
+
+            meta: C_meta = field(default_factory=C_meta)
+
+            @dataclass_json(undefined=Undefined.INCLUDE)
+            @dataclass
+            class C_rule_severities:
+                annular_width: str = "error"
+                clearance: str = "error"
+                connection_width: str = "warning"
+                copper_edge_clearance: str = "error"
+                copper_sliver: str = "warning"
+                courtyards_overlap: str = "error"
+                diff_pair_gap_out_of_range: str = "error"
+                diff_pair_uncoupled_length_too_long: str = "error"
+                drill_out_of_range: str = "error"
+                duplicate_footprints: str = "warning"
+                extra_footprint: str = "warning"
+                footprint: str = "error"
+                footprint_symbol_mismatch: str = "warning"
+                footprint_type_mismatch: str = "ignore"
+                hole_clearance: str = "error"
+                hole_near_hole: str = "error"
+                holes_co_located: str = "warning"
+                invalid_outline: str = "error"
+                isolated_copper: str = "warning"
+                item_on_disabled_layer: str = "error"
+                items_not_allowed: str = "error"
+                length_out_of_range: str = "error"
+                lib_footprint_issues: str = "warning"
+                lib_footprint_mismatch: str = "warning"
+                malformed_courtyard: str = "error"
+                microvia_drill_out_of_range: str = "error"
+                missing_courtyard: str = "ignore"
+                missing_footprint: str = "warning"
+                net_conflict: str = "warning"
+                npth_inside_courtyard: str = "ignore"
+                padstack: str = "warning"
+                pth_inside_courtyard: str = "ignore"
+                shorting_items: str = "error"
+                silk_edge_clearance: str = "warning"
+                silk_over_copper: str = "warning"
+                silk_overlap: str = "warning"
+                skew_out_of_range: str = "error"
+                solder_mask_bridge: str = "error"
+                starved_thermal: str = "error"
+                text_height: str = "warning"
+                text_thickness: str = "warning"
+                through_hole_pad_without_hole: str = "error"
+                too_many_vias: str = "error"
+                track_dangling: str = "warning"
+                track_width: str = "error"
+                tracks_crossing: str = "error"
+                unconnected_items: str = "error"
+                unresolved_variable: str = "error"
+                via_dangling: str = "warning"
+                zones_intersect: str = "error"
+                unknown: CatchAll = None
+
+            rule_severities: C_rule_severities = field(
+                default_factory=C_rule_severities
+            )
+
+            @dataclass_json(undefined=Undefined.INCLUDE)
+            @dataclass
+            class C_rules:
+                max_error: float = 0.005
+                min_clearance: float = 0.0
+                min_connection: float = 0.0
+                min_copper_edge_clearance: float = 0.5
+                min_hole_clearance: float = 0.25
+                min_hole_to_hole: float = 0.25
+                min_microvia_diameter: float = 0.2
+                min_microvia_drill: float = 0.1
+                min_resolved_spokes: int = 2
+                min_silk_clearance: float = 0.0
+                min_text_height: float = 0.8
+                min_text_thickness: float = 0.08
+                min_through_hole_diameter: float = 0.3
+                min_track_width: float = 0.0
+                min_via_annular_width: float = 0.1
+                min_via_diameter: float = 0.5
+                solder_mask_to_copper_clearance: float = 0.0
+                use_height_for_length_calcs: bool = True
+                unknown: CatchAll = None
+
+            rules: C_rules = field(default_factory=C_rules)
+
+            @dataclass_json(undefined=Undefined.INCLUDE)
+            @dataclass
+            class C_teardrop_options:
+                td_onpadsmd: bool = True
+                td_onroundshapesonly: bool = False
+                td_ontrackend: bool = False
+                td_onviapad: bool = True
+                unknown: CatchAll = None
+
+            teardrop_options: list[C_teardrop_options] = field(default_factory=list)
+
+            @dataclass_json(undefined=Undefined.INCLUDE)
+            @dataclass
+            class C_teardrop_parameters:
+                td_allow_use_two_tracks: bool = True
+                td_curve_segcount: int = 0
+                td_height_ratio: float = 1.0
+                td_length_ratio: float = 0.5
+                td_maxheight: float = 2.0
+                td_maxlen: float = 1.0
+                td_on_pad_in_zone: bool = False
+                td_target_name: str = "td_round_shape"
+                td_width_to_size_filter_ratio: float = 0.9
+                unknown: CatchAll = None
+
+            teardrop_parameters: list[C_teardrop_parameters] = field(
+                default_factory=list
+            )
+
+            @dataclass_json(undefined=Undefined.INCLUDE)
+            @dataclass
+            class C_track_widths:
+                unknown: CatchAll = None
+
+            track_widths: list[C_track_widths] = field(default_factory=list)
+
+            @dataclass_json(undefined=Undefined.INCLUDE)
+            @dataclass
+            class C_tuning_pattern_settings:
+                @dataclass_json(undefined=Undefined.INCLUDE)
+                @dataclass
+                class C_diff_pair_defaults:
+                    corner_radius_percentage: int = 80
+                    corner_style: int = 1
+                    max_amplitude: float = 1.0
+                    min_amplitude: float = 0.2
+                    single_sided: bool = False
+                    spacing: float = 1.0
+                    unknown: CatchAll = None
+
+                diff_pair_defaults: C_diff_pair_defaults = field(
+                    default_factory=C_diff_pair_defaults
+                )
+
+                @dataclass_json(undefined=Undefined.INCLUDE)
+                @dataclass
+                class C_diff_pair_skew_defaults:
+                    corner_radius_percentage: int = 80
+                    corner_style: int = 1
+                    max_amplitude: float = 1.0
+                    min_amplitude: float = 0.2
+                    single_sided: bool = False
+                    spacing: float = 0.6
+                    unknown: CatchAll = None
+
+                diff_pair_skew_defaults: C_diff_pair_skew_defaults = field(
+                    default_factory=C_diff_pair_skew_defaults
+                )
+
+                @dataclass_json(undefined=Undefined.INCLUDE)
+                @dataclass
+                class C_single_track_defaults:
+                    corner_radius_percentage: int = 80
+                    corner_style: int = 1
+                    max_amplitude: float = 1.0
+                    min_amplitude: float = 0.2
+                    single_sided: bool = False
+                    spacing: float = 0.6
+                    unknown: CatchAll = None
+
+                single_track_defaults: C_single_track_defaults = field(
+                    default_factory=C_single_track_defaults
+                )
+                unknown: CatchAll = None
+
+            tuning_pattern_settings: C_tuning_pattern_settings = field(
+                default_factory=C_tuning_pattern_settings
+            )
+
+            @dataclass_json(undefined=Undefined.INCLUDE)
+            @dataclass
+            class C_via_dimensions:
+                unknown: CatchAll = None
+
+            via_dimensions: list[C_via_dimensions] = field(default_factory=list)
+            zones_allow_external_fillets: bool = False
+            unknown: CatchAll = None
+
+        design_settings: C_design_settings = field(default_factory=C_design_settings)
+
+        @dataclass_json(undefined=Undefined.INCLUDE)
+        @dataclass
+        class C_ipc2581:
+            dist: str = ""
+            distpn: str = ""
+            internal_id: str = ""
+            mfg: str = ""
+            mpn: str = ""
+            unknown: CatchAll = None
+
+        ipc2581: C_ipc2581 = field(default_factory=C_ipc2581)
+
+        @dataclass_json(undefined=Undefined.INCLUDE)
+        @dataclass
+        class C_layer_presets:
+            unknown: CatchAll = None
+
+        layer_presets: list[C_layer_presets] = field(default_factory=list)
+
+        @dataclass_json(undefined=Undefined.INCLUDE)
+        @dataclass
+        class C_viewports:
+            unknown: CatchAll = None
+
+        viewports: list[C_viewports] = field(default_factory=list)
+        unknown: CatchAll = None
+
+    board: C_board = field(default_factory=C_board)
+
+    @dataclass_json(undefined=Undefined.INCLUDE)
+    @dataclass
+    class C_boards:
+        unknown: CatchAll = None
+
+    boards: list[C_boards] = field(default_factory=list)
+
+    @dataclass_json(undefined=Undefined.INCLUDE)
+    @dataclass
+    class C_cvpcb:
+        @dataclass_json(undefined=Undefined.INCLUDE)
+        @dataclass
+        class C_equivalence_files:
+            unknown: CatchAll = None
+
+        equivalence_files: list[C_equivalence_files] = field(default_factory=list)
+        unknown: CatchAll = None
+
+    cvpcb: C_cvpcb = field(default_factory=C_cvpcb)
+
+    @dataclass_json(undefined=Undefined.INCLUDE)
+    @dataclass
+    class C_libraries:
+        @dataclass_json(undefined=Undefined.INCLUDE)
+        @dataclass
+        class C_pinned_footprint_libs:
+            unknown: CatchAll = None
+
+        pinned_footprint_libs: list[C_pinned_footprint_libs] = field(
+            default_factory=list
+        )
+
+        @dataclass_json(undefined=Undefined.INCLUDE)
+        @dataclass
+        class C_pinned_symbol_libs:
+            unknown: CatchAll = None
+
+        pinned_symbol_libs: list[C_pinned_symbol_libs] = field(default_factory=list)
+        unknown: CatchAll = None
+
+    libraries: C_libraries = field(default_factory=C_libraries)
+
+    @dataclass_json(undefined=Undefined.INCLUDE)
+    @dataclass
+    class C_meta:
+        filename: str = "example.kicad_pro"
+        version: int = 1
+        unknown: CatchAll = None
+
+    meta: C_meta = field(default_factory=C_meta)
+
+    @dataclass_json(undefined=Undefined.INCLUDE)
+    @dataclass
+    class C_net_settings:
+        @dataclass_json(undefined=Undefined.INCLUDE)
+        @dataclass
+        class C_classes:
+            bus_width: int = 12
+            clearance: float = 0.2
+            diff_pair_gap: float = 0.25
+            diff_pair_via_gap: float = 0.25
+            diff_pair_width: float = 0.2
+            line_style: int = 0
+            microvia_diameter: float = 0.3
+            microvia_drill: float = 0.1
+            name: str = "Default"
+            pcb_color: str = "rgba(0, 0, 0, 0.000)"
+            schematic_color: str = "rgba(0, 0, 0, 0.000)"
+            track_width: float = 0.2
+            via_diameter: float = 0.6
+            via_drill: float = 0.3
+            wire_width: int = 6
+            unknown: CatchAll = None
+
+        classes: list[C_classes] = field(default_factory=list)
+
+        @dataclass_json(undefined=Undefined.INCLUDE)
+        @dataclass
+        class C_meta:
+            version: int = 3
+            unknown: CatchAll = None
+
+        meta: C_meta = field(default_factory=C_meta)
+        net_colors: Optional[Any] = None
+        netclass_assignments: Optional[Any] = None
+        netclass_patterns: list = field(default_factory=list)
+        unknown: CatchAll = None
+
+    net_settings: C_net_settings = field(default_factory=C_net_settings)
+
+    @dataclass_json(undefined=Undefined.INCLUDE)
+    @dataclass
+    class C_schematic:
+        legacy_lib_dir: str = ""
+        legacy_lib_list: list = field(default_factory=list)
+        unknown: CatchAll = None
+
+    schematic: C_schematic = field(default_factory=C_schematic)
+
+    @dataclass_json(undefined=Undefined.INCLUDE)
+    @dataclass
+    class C_sheets:
+        unknown: CatchAll = None
+
+    sheets: list[C_sheets] = field(default_factory=list)
+
+    @dataclass_json(undefined=Undefined.INCLUDE)
+    @dataclass
+    class C_text_variables:
+        unknown: CatchAll = None
+
+    text_variables: dict = field(default_factory=dict)
     unknown: CatchAll = None
-
-
-# ---------------------------------------------------------------------
-# Kicad SEXP subset
-# only atom order is important
-# multi-key dict
 
 
 class UUID(str):

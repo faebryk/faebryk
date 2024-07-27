ffbcf72d8fb23c3ec285d7110ac083cf4b9c1666
ruben-iteng <94007802+ruben-iteng@users.noreply.github.com>
2024-07-27 17:56:53 +0200

    Add: txt or markdown format

--- src/faebryk/exporters/parameters/parameters_to_file.py
+++ src/faebryk/exporters/parameters/parameters_to_file.py
@@ -19,22 +19,45 @@ def export_parameters_to_file(module: Module, path: Path):
     for m in {
         _m.get_most_special() for _m in get_all_modules(module.get_most_special())
     }:
-        parameters[m.get_full_name(types=True)] = [
+        parameters[m.get_full_name(types=True).split(".", maxsplit=1)[-1]] = [
             {param.get_full_name().split(".")[-1]: param}
             for param in m.PARAMs.get_all()
         ]
 
     logger.info(f"Writing parameters to {path}")
-    with open(path, "w") as f:
-        for module_name, paras in sorted(parameters.items()):
-            if paras:
-                f.write(f"{module_name}\n")
-                f.writelines(
-                    [
-                        f"    {par_name}: {par_value}\n"
-                        for par_dict in paras
-                        for par_name, par_value in par_dict.items()
-                    ]
-                )
-                f.write("\n")
-        f.close()
+    if path.suffix == ".txt":
+        with open(path, "w") as f:
+            for module_name, paras in sorted(parameters.items()):
+                if paras:
+                    f.write(f"{module_name}\n")
+                    f.writelines(
+                        [
+                            f"    {par_name}: {par_value}\n"
+                            for par_dict in paras
+                            for par_name, par_value in par_dict.items()
+                        ]
+                    )
+                    f.write("\n")
+            f.close()
+    elif path.suffix == ".md":
+        with open(path, "w") as f:
+            f.write("# Module Parameters\n")
+            for module_name, paras in sorted(parameters.items()):
+                if paras:
+                    f.write(f"**{module_name.replace("|","&#124;")}**\n")
+                    f.write("| Parameter Name | Parameter Value |\n")
+                    f.write("| --- | --- |\n")
+                    f.writelines(
+                        [
+                            f"| {par_name.replace("|","&#124;")} | {str(par_value).replace("|","&#124;")} |\n"  # noqa E501
+                            for par_dict in paras
+                            for par_name, par_value in par_dict.items()
+                        ]
+                    )
+                    f.write("\n")
+            f.write("\n")
+            f.close()
+    else:
+        AssertionError(
+            f"Export to file extension [{path.suffix}] not supported in {path}"
+        )

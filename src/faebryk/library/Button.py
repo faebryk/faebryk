7d37e240ecfa170da824897a8b848086c1286cd8
ruben-iteng <94007802+ruben-iteng@users.noreply.github.com>
2024-08-16 10:50:33 +0200

    Fix: designator
    Add: height parameter

--- src/faebryk/library/Button.py
+++ src/faebryk/library/Button.py
@@ -7,6 +7,7 @@ from faebryk.core.core import Module
 from faebryk.library.can_bridge_defined import can_bridge_defined
 from faebryk.library.Electrical import Electrical
 from faebryk.library.has_designator_prefix_defined import has_designator_prefix_defined
+from faebryk.library.TBD import TBD
 from faebryk.libs.util import times
 
 logger = logging.getLogger(__name__)
@@ -25,10 +26,11 @@ class Button(Module):
 
         self.IFs = _IFs(self)
 
-        class _PARAMs(Module.PARAMS()): ...
+        class _PARAMs(Module.PARAMS()):
+            height = TBD[float]()
 
         self.PARAMs = _PARAMs(self)
 
-        self.add_trait(has_designator_prefix_defined("S"))
+        self.add_trait(has_designator_prefix_defined("SW"))
 
         self.add_trait(can_bridge_defined(self.IFs.unnamed[0], self.IFs.unnamed[1]))
